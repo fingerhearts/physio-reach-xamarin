76dc36a6e1ee65b2d627e2a13291f246cc441030
Alejandroid101 <kik.rdl3@gmail.com>
2019-12-16 16:16:25 -0800

    Home page  to exercise1 page to results page.

---
+++ dotnetfinal/dotnetfinal/dotnetfinal/Results.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace dotnetfinal
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Results : ContentPage
+    {
+        public Results()
+        {
+            InitializeComponent();
+        }
+    }
+}
