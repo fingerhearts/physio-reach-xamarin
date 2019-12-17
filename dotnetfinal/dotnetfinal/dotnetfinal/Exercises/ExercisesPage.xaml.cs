b7957781f9f586b3435728af21efd29abcbd9119
Alejandroid101 <kik.rdl3@gmail.com>
2019-12-17 14:59:06 -0800

    Added exercises page between each exercise and main page

---
+++ dotnetfinal/dotnetfinal/dotnetfinal/Exercises/ExercisesPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace dotnetfinal.Exercises
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExercisesPage : ContentPage
+    {
+        public ExercisesPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked1(object sender, EventArgs e)
+        {
+            //Button button = sender as Button;
+            await Navigation.PushAsync(new Exercises.Exercise1());
+        }
+        async void ButtonClicked2(object sender, EventArgs e)
+        {
+            //Button button = sender as Button;
+            await Navigation.PushAsync(new Exercises.Exercise2());
+        }
+        async void ButtonClicked3(object sender, EventArgs e)
+        {
+            //Button button = sender as Button;
+            await Navigation.PushAsync(new Exercises.Exercise3());
+        }
+    }
+}
