eb88c7e1a2ea8bc20eaf916806c0c3b50ea18d73
Karina Chen <karina6188@gmail.com>
2019-12-17 18:48:10 -0800

    Link timer with StopButton function when count down to zero

--- dotnetfinal/dotnetfinal/dotnetfinal/Exercises/Exercise1.xaml.cs
+++ dotnetfinal/dotnetfinal/dotnetfinal/Exercises/Exercise1.xaml.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Android.Media;
+using Android.OS;
+using Plugin.SimpleAudioPlayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,19 +20,21 @@ namespace dotnetfinal.Exercises
     {
         public List<Coordinate> Coordinates { get; set; } = new List<Coordinate>();
 
+        ISimpleAudioPlayer player;
+
         public Exercise1()
         {
             InitializeComponent();
         }
 
-        public void Countdown(bool running)
+        public void Countdown()
         {
             int _SecondsElapsed = 8;
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
                 start.Text = "Running";
                 LabelX.Text = "";
-                while (_SecondsElapsed >= 0)
+            while (_SecondsElapsed >= 0)
                 {
                     switch (_SecondsElapsed)
                     {
@@ -52,14 +59,13 @@ namespace dotnetfinal.Exercises
                 LabelX.Text = "Stopped";
                 start.Text = "Start";
                 countdownTimer.Text = "";
-                return false;
+            return false;
             });
         }
 
-
         void StartButton(object sender, EventArgs e)
         {
-            Countdown(true);
+            Countdown();
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
             Accelerometer.Start(SensorSpeed.UI);
         }
@@ -67,7 +73,7 @@ namespace dotnetfinal.Exercises
         private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
             Coordinate coordinate = new Coordinate();
-            
+
             coordinate.XValue = Math.Round(Convert.ToDouble(e.Reading.Acceleration.X) * 100 / 2.54, 2);
             coordinate.YValue = Math.Round(Convert.ToDouble(e.Reading.Acceleration.Y) * 100 / 2.54, 2);
             coordinate.ZValue = Math.Round(Convert.ToDouble(e.Reading.Acceleration.Z) * 100 / 2.54, 2);
@@ -81,5 +87,6 @@ namespace dotnetfinal.Exercises
             //Button button = sender as Button;
             await Navigation.PushAsync(new Results(Coordinates) { Title = "Results", BindingContext = vData });
         }
+
     }
 }
